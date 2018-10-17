e29e9156c146fa3cd5b916fc94b03a26f3a0870f
其实我叫有人 <42315439+xiang59915@users.noreply.github.com>
2018-10-17 16:08:00 +0800

    Update 通用爬虫.py

--- 通用爬虫.py
+++ 通用爬虫.py
@@ -3,7 +3,7 @@ import json
 
 class jandan_spider():
     def __init__(self):
-        self.start_url = "http://i.jandan.net/pic/page-{}#comments" #煎蛋无聊图板块{}用于填写页数
+        self.start_url = "http://i.jandan.net/pic/page-{num}#comments" #煎蛋无聊图板块{}用于填写页数
         self.headers = {"User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Mobile Safari/537.36"} # 模拟安卓手机访问
 
     def parse_url(self, url):
