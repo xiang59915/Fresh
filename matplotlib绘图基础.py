4dafe5feba071e056d3090df93c6be3f9f35265b
其实我叫有人 <42315439+xiang59915@users.noreply.github.com>
2018-10-18 18:25:04 +0800

    Update matplotlib绘图基础.py

--- matplotlib绘图基础.py
+++ matplotlib绘图基础.py
@@ -1,13 +1,24 @@
 from matplotlib import pyplot as plt
+from matplotlib import font_manager
 
-x = (0, 30, 2)
-y = [11, 22, 33, 44, 11, 22, 33, 44, 8, 9, 55, 66, 44, 33, 88]
-#设置图形大小
-plt.figure(figuresize = (15, 15) dpi = 80)
-# 绘制折线图  其他类型后续补充
-plt.plot(x, y)
-plt.xticks(font_manager=my_font)
-# 保存
-plt.savefig("路径.png")
-# 弹框
+# 选用一个图表通用字体存为my_font，为显示中文
+my_font = font_manager.FontProperties(fname="字体文件所在目录")
+
+# 数据
+x = range(11)
+y = [1, 5, 6, 2, 11, 8, 9, 23, 5, 6, 8]
+
+# 绘制折线图
+plt.plot(x, y, label="图例名", color="折线颜色")
+# 设置图形大小
+plt.figure(figsize=(20,8),dpi=80)
+
+# 设置坐标
+_xtick_label = ["{}年".format(i) for i in x]
+plt.xticks(x, _xtick_label, fontproperties=my_font)
+
+# 绘制网格
+plt.grid(alpha= 0.4, linestyle="--")
+
+# 展示
 plt.show()
