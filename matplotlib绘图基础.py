4eb88a989bd4b89e65d29d26fb0f261b2997dda9
其实我叫有人 <42315439+xiang59915@users.noreply.github.com>
2018-10-18 17:50:03 +0800

    Update matplotlib绘图基础.py

--- matplotlib绘图基础.py
+++ matplotlib绘图基础.py
@@ -4,8 +4,9 @@ x = (0, 30, 2)
 y = [11, 22, 33, 44, 11, 22, 33, 44, 8, 9, 55, 66, 44, 33, 88]
 #设置图形大小
 plt.figure(figuresize = (15, 15) dpi = 80)
-# 绘图
+# 绘制折线图  其他类型后续补充
 plt.plot(x, y)
+plt.xticks
 # 保存
 plt.savefig("路径.png")
 # 弹框
