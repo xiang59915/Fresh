e22fcf0eb03a68d7179027a6c3ee5edac3341a3d
其实我叫有人 <xiang59915@users.noreply.github.com>
2018-11-11 20:43:47 +0800

    Update FileTransferServer.py

--- FileTransferServer.py
+++ FileTransferServer.py
@@ -1,20 +1,33 @@
+# xiang59915练习
 import socket as s   # 导入socket模块
+import os
 
+server = s.socket()   # 创建套接字
 PORT = 2333   # 设置端口号为2333
-HOST = s.gethostname()   # 获取本地IP地址
-s.socket.bind((HOST, PORT))
-s.socket.listen()
+server.bind(("localhost", PORT))
+server.listen(8)
 
-print("FTP 等待接收...")
+print("等待客户端连接...")
 
 while True:
-	conn, addr = s.socket.accept()
+	conn, addr = server.accept()
 	print("客户端的IP地址和端口信息",addr)
 	data = conn.recv(1024)
-	print("客户端发过来的文件",repr(data))
+	print("客户端回应",repr(data))
 
-	FileName = input("输入一个文件名用于接收客户端发过来的文件例如test.txt")
+	FileName = os.path.basename(os.path.realpath(__file__))
 	f = open(FileName, 'rb')
-	
+	InData = f.read(1024)
+	while InData:
+		conn.send(InData)
+		print('sent', repr(InData))
+		InData = f.read(1024)
+	f.close()
+
+	print('传输完成')
+	conn.send("感谢使用本次FTP服务")
+	conn.close()
+
+
 
 
